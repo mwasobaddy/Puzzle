92438ece30f3c1d39fbe2901387e923da779385d
Hiikey7 <kelvinramsiel@gmail.com>
2026-03-12 14:53:07 +0300

    Configure Firebase settings and set up CORS rules

--- puzzle_one/src/firebase.js
+++ puzzle_one/src/firebase.js
@@ -1,49 +1,54 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
-// import { getDatabase, ref, set, onValue, update, remove, onDisconnect, push, runTransaction, get, increment } from 'firebase/database';
-// import { nanoid } from 'nanoid';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { initializeFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getDatabase, ref, set, onValue, update, remove, onDisconnect, push, runTransaction, get, increment } from 'firebase/database';
+import { nanoid } from 'nanoid';
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAuzyxG9Cs1ma9chjR-uJZegoMAc1Vp2Ig",
-//   authDomain: "nash-ac5c0.firebaseapp.com",
-//   projectId: "nash-ac5c0",
-//   storageBucket: "nash-ac5c0.firebasestorage.app",
-//   messagingSenderId: "49955314335",
-//   appId: "1:49955314335:web:e12140aa04351c658060aa",
-//   measurementId: "G-Y1LW4LFGR2",
-//   databaseURL: "https://nash-ac5c0-default-rtdb.firebaseio.com/",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyB3TF4ZHqHumrUDSyxaR7lNQzS-43PPK9s",
+  authDomain: "mwasobaddy.firebaseapp.com",
+  projectId: "mwasobaddy",
+  storageBucket: "mwasobaddy.firebasestorage.app",
+  messagingSenderId: "108027559981",
+  appId: "1:108027559981:web:44a95c4cf2246c742bf8a4",
+  measurementId: "G-B2DW15NH30",
+  databaseURL: "https://mwasobaddy-default-rtdb.firebaseio.com/",
+};
 
 
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const googleProvider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-// const storage = getStorage(app);
-// const db = getFirestore(app);
-// const database = getDatabase(app);
+const storage = getStorage(app);
+// experimentalForceLongPolling uses plain HTTP long-polling instead of
+// gRPC-web WebChannel. This is required for Brave / Safari where the
+// WebChannel transport is blocked, causing all Firestore writes to hang.
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+const database = getDatabase(app);
 
-// export {
-//   auth,
-//   googleProvider,
-//   storage,
-//   app,
-//   remove,
-//   push,
-//   db,
-//   database,
-//   onDisconnect,
-//   ref,
-//   set,
-//   onValue,
-//   update,
-//   nanoid,
-//   runTransaction,
-//   get,
-//   increment
-// };
+export {
+  auth,
+  googleProvider,
+  storage,
+  app,
+  remove,
+  push,
+  db,
+  database,
+  onDisconnect,
+  ref,
+  set,
+  onValue,
+  update,
+  nanoid,
+  runTransaction,
+  get,
+  increment
+};
 
 
